0fa7f3fd6f28b371c07c3b99ac207bc50b37856d
aviadhahami <aviadhahami@gmail.com>
2014-06-22 18:07:52 +0300

    Managed to connect
    Connection between devices established, sockets need implementation

--- Project/src/com/AviadHahami/flickit/MainScreen.java
+++ Project/src/com/AviadHahami/flickit/MainScreen.java
@@ -1,17 +1,60 @@
 package com.AviadHahami.flickit;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
+import android.widget.TextView;
 
 public class MainScreen extends Activity {
+
+	WifiP2pManager mManager;
+	Channel mChannel;
+	BroadcastReceiver mReceiver;
+	IntentFilter mIntentFilter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_screen);
+
+		TextView text = (TextView) findViewById(R.id.loggerTxt);
+		text.setText(android.os.Build.DEVICE);
+		mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+		mChannel = mManager.initialize(this, getMainLooper(), null);
+		mReceiver = new MyReceiver(mManager, mChannel, this);
+
+		mIntentFilter = new IntentFilter();
+		mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+		mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+		mIntentFilter
+				.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+		mIntentFilter
+				.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+		discoverPeers();
+	}
+
+	private void discoverPeers() {
+		mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+
+			@Override
+			public void onSuccess() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onFailure(int reason) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
 	}
 
 	@Override
@@ -21,12 +64,18 @@ public class MainScreen extends Activity {
 		return true;
 	}
 
-	// Broadcasting intent
-	public void broadcastIntent(View view) {
-		Intent intent = new Intent();
-		intent.setAction("com.AviadHahami.SEND_DATA");
-		intent.putExtra("sentData", "Some data goes here");
-		sendBroadcast(intent);
+	/* register the broadcast receiver with the intent values to be matched */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		registerReceiver(mReceiver, mIntentFilter);
+	}
+
+	/* unregister the broadcast receiver */
+	@Override
+	protected void onPause() {
+		super.onPause();
+		unregisterReceiver(mReceiver);
 	}
 
 }
--- Project/src/com/AviadHahami/flickit/MyReceiver.java
+++ Project/src/com/AviadHahami/flickit/MyReceiver.java
@@ -4,28 +4,116 @@
 
 package com.AviadHahami.flickit;
 
+import java.util.Iterator;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.ActionListener;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.widget.Toast;
 
 public class MyReceiver extends BroadcastReceiver {
 
+	private WifiP2pManager mManager;
+	private Channel mChannel;
+	private MainScreen mActivity;
+	private WifiP2pDeviceList peers;
+
+	public MyReceiver(WifiP2pManager manager, Channel channel,
+			MainScreen activity) {
+		super();
+		this.mManager = manager;
+		this.mChannel = channel;
+		this.mActivity = activity;
+	}
+
 	@Override
-	public void onReceive(Context context, Intent intent) {
+	public void onReceive(final Context context, Intent intent) {
 
 		Bundle incoming = intent.getExtras();
 		String incomingData = "";
 		if (incoming != null) {
 			incomingData = incoming.getString("sentData");
 		}
-		Toast.makeText(context, incomingData, Toast.LENGTH_LONG).show();
-		Vibrator v = (Vibrator) context
-				.getSystemService(Context.VIBRATOR_SERVICE);
-		// Vibrate for 500 milliseconds
-		v.vibrate(500);
+
+		String action = intent.getAction();
+
+		if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+			int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+			if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+				// Wifi P2P is enabled
+				Toast.makeText(context, "Wifi P2P enabled", Toast.LENGTH_LONG)
+						.show();
+			} else {
+				// Wi-Fi P2P is not enabled
+				Toast.makeText(context, "Wifi P2P disabled", Toast.LENGTH_LONG)
+						.show();
+			}
+		}
+
+		if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+			// Check to see if Wi-Fi is enabled and notify appropriate activity
+		} else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+			// Call WifiP2pManager.requestPeers() to get a list of current peers
+			Toast.makeText(context, action, Toast.LENGTH_LONG).show();
+			if (mManager != null) {
+				mManager.requestPeers(mChannel, new PeerListListener() {
+
+					@Override
+					public void onPeersAvailable(WifiP2pDeviceList peers) {
+						Toast.makeText(
+								context,
+								"peers available "
+										+ peers.getDeviceList().size(),
+								Toast.LENGTH_LONG).show();
+						WifiP2pDevice myDevice = null;
+						for (Iterator<WifiP2pDevice> iterator = peers
+								.getDeviceList().iterator(); iterator.hasNext();) {
+							myDevice = iterator.next();
+						}
+						connectToDevice(myDevice);
+					}
+				});
+			}
+
+		} else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION
+				.equals(action)) {
+			// Respond to new connection or disconnections
+		} else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION
+				.equals(action)) {
+			// Respond to this device's wifi state changing
+		}
+	}
+
+	protected void connectToDevice(WifiP2pDevice myDevice) {
+		WifiP2pConfig config = new WifiP2pConfig();
+		config.deviceAddress = myDevice.deviceAddress;
+		mManager.connect(mChannel, config, new ActionListener() {
+
+			@Override
+			public void onSuccess() {
+				Toast.makeText(mActivity, "Did connect !", Toast.LENGTH_LONG)
+						.show();
+
+			}
+
+			@Override
+			public void onFailure(int reason) {
+				Toast.makeText(mActivity, "Did not connect !",
+						Toast.LENGTH_LONG).show();
+			}
+		});
+
+	
+
 	}
 
 }
