1fdb51025a4b385bbdbfd9676c0a80c1bb4663fd
aviadhahami <aviadhahami@gmail.com>
2014-06-22 18:08:48 +0300

    same commint

---
+++ Project/src/com/AviadHahami/flickit/Sockets.java
@@ -0,0 +1,17 @@
+/**
+@author AviadHahami
+*/
+
+package com.AviadHahami.flickit;
+
+import android.os.AsyncTask;
+
+public class Sockets extends AsyncTask {
+	
+	@Override
+	protected Object doInBackground(Object... arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
