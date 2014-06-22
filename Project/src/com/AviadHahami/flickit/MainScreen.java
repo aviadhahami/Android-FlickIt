623da0f8c71190b01d9f96966b5561d38ee0fd9e
aviadhahami <aviadhahami@gmail.com>
2014-06-22 15:24:16 +0300

    Revert "Tryin to understand peers"
    This reverts commit 1f6042d7c8619130ee3d2560ae967e31b0d8822d.

--- Project/src/com/AviadHahami/flickit/MainScreen.java
+++ Project/src/com/AviadHahami/flickit/MainScreen.java
@@ -1,80 +1,17 @@
 package com.AviadHahami.flickit;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.wifi.p2p.WifiP2pConfig;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pManager;
-import android.net.wifi.p2p.WifiP2pManager.ActionListener;
-import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainScreen extends Activity {
-
-	static WifiP2pManager mManager;
-	Channel mChannel;
-	BroadcastReceiver mReceiver;
-	IntentFilter mIntentFilter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_screen);
-
-		mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-		mChannel = mManager.initialize(this, getMainLooper(), null);
-		mReceiver = new MyReceiver(mManager, mChannel, this);
-
-		mIntentFilter = new IntentFilter();
-		mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-		mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-		mIntentFilter
-				.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-		mIntentFilter
-				.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-		// Lets get the peers
-		mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
-			@Override
-			public void onSuccess() {
-				// TODO: implement
-				Toast.makeText(getBaseContext(), "got peers", Toast.LENGTH_LONG)
-						.show();
-			}
-
-			@Override
-			public void onFailure(int reasonCode) {
-				// TODO: implement
-				Toast.makeText(getBaseContext(), "Peers failed: " + reasonCode,
-						Toast.LENGTH_LONG).show();
-			}
-		});
-
-		WifiP2pDevice device = new WifiP2pDevice();
-		WifiP2pConfig config = new WifiP2pConfig();
-		config.deviceAddress = device.deviceAddress;
-		mManager.connect(mChannel, config, new ActionListener() {
-
-			@Override
-			public void onSuccess() {
-				// success logic
-				Toast.makeText(getBaseContext(), "connection success",
-						Toast.LENGTH_LONG).show();
-			}
-
-			@Override
-			public void onFailure(int reason) {
-				Toast.makeText(getBaseContext(), "connection failure",
-						Toast.LENGTH_LONG).show();
-			}
-		});
-
 	}
 
 	@Override
@@ -84,24 +21,11 @@ public class MainScreen extends Activity {
 		return true;
 	}
 
-	/* register the broadcast receiver with the intent values to be matched */
-	@Override
-	protected void onResume() {
-		super.onResume();
-		registerReceiver(mReceiver, mIntentFilter);
-	}
-
-	/* unregister the broadcast receiver */
-	@Override
-	protected void onPause() {
-		super.onPause();
-		unregisterReceiver(mReceiver);
-	}
-
 	// Broadcasting intent
 	public void broadcastIntent(View view) {
 		Intent intent = new Intent();
 		intent.setAction("com.AviadHahami.SEND_DATA");
+		intent.putExtra("sentData", "Some data goes here");
 		sendBroadcast(intent);
 	}
 
